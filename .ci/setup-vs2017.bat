18ebf3ede53ae0cb892b9a4481a1ca3d01b3dfe8
Nuno Subtil <nsubtil@nvidia.com>
2019-04-22 21:50:58 -0500

    Clean up appveyor

--- .ci/setup-vs2017.bat
+++ .ci/setup-vs2017.bat
@@ -1,10 +1,8 @@
 @rem sets up env variables for installed MSVC2017
 @rem based on https://github.com/Microsoft/vswhere/wiki/Examples
 
-set VSCMD_START_DIR=%CI_PROJECT_DIR%
-
 for /f "usebackq tokens=1* delims=: " %%i in (`%CI_PROJECT_DIR%/.ci/vswhere.exe -version 15 -requires Microsoft.VisualStudio.Workload.NativeDesktop`) do (
     if /i "%%i"=="installationPath" set VS2017DIR=%%j
 )
 
-call "%VS2017DIR%\Common7\Tools\VsDevCmd.bat" -arch=%BUILD_ARCH%
+call "%VS2017DIR%\Common7\Tools\VsDevCmd.bat" -arch=%platform% -startdir=none
