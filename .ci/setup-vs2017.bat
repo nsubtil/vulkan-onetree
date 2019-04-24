780fd46324f1e7de9cc8dee924504af23342b994
Nuno Subtil <nsubtil@nvidia.com>
2019-04-24 14:28:17 +0800

    Properly detect VS2015

--- .ci/setup-vs2017.bat
+++ .ci/setup-vs2017.bat
@@ -1,8 +1,8 @@
 @rem sets up env variables for installed MSVC2017
 @rem based on https://github.com/Microsoft/vswhere/wiki/Examples
 
-for /f "usebackq tokens=1* delims=: " %%i in (`%CI_PROJECT_DIR%/.ci/vswhere.exe -version 15 -requires Microsoft.VisualStudio.Workload.NativeDesktop`) do (
+for /f "usebackq tokens=1* delims=: " %%i in (`%APPVEYOR_BUILD_FOLDER%/.ci/vswhere.exe -version 15 -requires Microsoft.VisualStudio.Workload.NativeDesktop`) do (
     if /i "%%i"=="installationPath" set VS2017DIR=%%j
 )
 
-call "%VS2017DIR%\Common7\Tools\VsDevCmd.bat" -arch=%platform% -startdir=none
+call "%VS2017DIR%\Common7\Tools\VsDevCmd.bat" -arch=%arch% -startdir=none
